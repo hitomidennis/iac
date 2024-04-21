b713f5cb9d60e58e11f25564ee79da0c4e0f7bed
hitomidennis <hitomi.dennis@gmail.com>
2024-04-21 16:18:03 +0800

    change profile

--- terraform/iam-instance-profile.tf
+++ terraform/iam-instance-profile.tf
@@ -1,4 +1,4 @@
 resource "aws_iam_instance_profile" "instance-profile" {
-  name = "Jumphost-instance-profile"
+  name = "Jumphost-instance-profile-us-west-2"
   role = aws_iam_role.iam-role.name
 }
